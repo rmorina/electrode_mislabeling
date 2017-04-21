84c9f7b9b4fe16925e904c1b17d5e017f1a3e220
rachel <sunsunruirui@gmail.com>
2017-04-21 13:54:15 -0400

    add_noise function

--- sEEG_forward_model_simulation/add_noise.m
+++ sEEG_forward_model_simulation/add_noise.m
@@ -1,6 +1,19 @@
-%% add noise
-noise_snr=20;
-ideal_norm=norm(groundTruthMeasure(:));
-noise_norm=sum(rand(elec_num,1).^2)^0.5;
-var=10^(-noise_snr/20)*ideal_norm/noise_norm;
-elec_signal=groundTruthMeasure + rand(elec_num,1)*var;
+function x_noisy = add_noise(x, noise_snr)
+    % Tiny wrapper to add AWGN noise for a given SNR.
+    %
+    % Inputs:
+    %   x: Signal of interest.
+    %   noise_snr: SNR of noise to add.
+    % 
+    % Outputs:
+    %   x_noisy: Noisy version of x.
+    
+    noise = randn(size(x));
+    
+    noise_norm = norm(noise(:));
+    xnorm = norm(x(:));
+    
+    noise = noise*10^(-noise_snr/20)*xnorm/noise_norm;
+    
+    x_noisy = x + noise;
+end
