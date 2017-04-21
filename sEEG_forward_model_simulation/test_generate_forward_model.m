3e75a3aba1de5f1bc1f923ebe4389c08c8eebedf
Rudina Morina <rudinamorina@Rudinas-MBP.wv.cc.cmu.edu>
2017-04-21 11:03:41 -0400

    Classification first run

--- sEEG_forward_model_simulation/test_generate_forward_model.m
+++ sEEG_forward_model_simulation/test_generate_forward_model.m
@@ -1,6 +1,19 @@
 % test_generate_forward_model
-for i = 20:20
-    eeg_location = generateEEGLocation(i);
-    depth_electrode_location = generateSEEGLocation(3);
-    A = generateForwardModel(depth_electrode_location, eeg_location);
+% Parameters
+eeg_num = 20;
+seeg_num = 3;
+sigma_squared = 5;
+sim_per_depth_electrode = 100;
+
+eeg_location = generateEEGLocation(eeg_num);
+depth_electrode_location = generateSEEGLocation(seeg_num);
+A = generateForwardModel(depth_electrode_location, eeg_location);
+[d_train, d_test, labels] = generateRecordings(A, sigma_squared, sim_per_depth_electrode);
+
+% Classification
+[ mus, sigmas, pis ] = fitGaussianModel(d_train, labels, seeg_num);
+test_labels = nan(1, size(d_test, 2));
+for i = 1 : size(d_test, 2)
+    cur_test_point = d_test(:, i);
+    test_labels(i) = findLabel(mus, sigmas, pis, cur_test_point);
 end
