8a9142e4ed1ca59dd1ad02395bf828e78c901029
Rudina Morina <rudinamorina@Rudinas-MacBook-Pro.local>
2017-04-21 10:15:30 -0400

    Gaussian classification

---
+++ sEEG_forward_model_simulation/fitGaussianModel.m
@@ -0,0 +1,18 @@
+function [ mus, sigmas, pis ] = fitGaussianModel(X, Y, class_number)
+    % Fit a Gaussian model for each class. Each class is modeled as a 
+    % Gaussian with a mean and covariance. So the parameters that need to 
+    % be estimated in this function are the mus and sigmas for each class.
+    sigmas = cell(class_number, 1);
+    mus = cell(class_number, 1);
+    pis = nan(1, class_number);
+    for i = 1 : class_number
+        cur_class_ind = Y == i;
+        class_training_points = X(:, cur_class_ind);
+        class_mu = mean(class_training_points);
+        class_sigma = cov(class_training_points');
+        pis(i) = size(class_training_points, 2)/size(X, 2);
+        sigmas{i, 1} = class_sigma;
+        mus{i, 1} = class_mu;
+    end
+end
+
